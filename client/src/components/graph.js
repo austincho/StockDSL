9704cb2beca74ba5e7b4e5b9eeff859c289d9bcf
austincho <austin.cho14@gmail.com>
2020-05-27 20:39:59 -0700

    bar graph function

--- client/src/components/graph.js
+++ client/src/components/graph.js
@@ -5,13 +5,30 @@ import * as d3 from "d3";
 class Graph extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: "",
+            type : "",
+            currency : "USD",
+            exchangeRate: 1
+        }
     }
-    createGraph(data) {
 
+    init(){
+        let dataset = this.organizeData(this.state.data, this.state.type);
+        if (this.state.type === "line") {
+            this.createLineGraph(dataset);
+        } else if (this.state.type === "bar") {
+            this.createBarGraph(dataset);
+        }
+    }
+
+    // }
+    organizeData(data, type) {
         let items = data[Object.keys(data)[1]];
 
         let dataset = [];
-
+        
+        if (type === "line") {
         Object.keys(items).forEach(function(key){
             let dataForDay = items[key];
             dataset.push({
@@ -21,8 +38,26 @@ class Graph extends Component {
                 "low" : parseFloat(dataForDay["3. low"]),
                 "close" : parseFloat(dataForDay["4. close"]),
                 "volume" : parseFloat(dataForDay["5. volume"])
+            });
         });
+    } else if (type === "line") {
+        var i = 0;
+        Object.keys(items).forEach(function(key){
+            if (i < 30) {
+            let dataForDay = items[key];
+            dataset.push({
+                "date": key.substring(5),
+                "open" : parseFloat(dataForDay["1. open"]),
+                "high" : parseFloat(dataForDay["2. high"])
+            });
+            }
+            i+=1;
         });
+    } 
+        return dataset.reverse();
+    }
+
+    createLineGraph(dataset) {        
 
         var optwidth        = 900;
         var optheight       = 500;
@@ -56,6 +91,14 @@ class Graph extends Component {
         .attr("transform", "translate(0," +height + ")")
         .call(d3.axisBottom(x));
 
+         // text label for the x axis
+        svg.append("text")             
+        .attr("transform",
+                "translate(" + (width/2) + " ," + 
+                            (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("Date");
+
             // Add Y axis
         var y = d3.scaleLinear()
             .domain(dataYrange)
@@ -63,6 +106,14 @@ class Graph extends Component {
 
             svg.append("g")
             .call(d3.axisLeft(y));
+
+            svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x",0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Price in USD");      
 
             // Add the line
         svg.append("path")
@@ -74,6 +125,55 @@ class Graph extends Component {
             .x(function(d) { return x(d.date) })
             .y(function(d) { return y(d.high) })
             )
+    }
+
+    
+    createBarGraph(dataset){
+
+        var optwidth        = 1200;
+        var optheight       = 500;
+
+        var margin	= {top: 20, right: 20, bottom: 30, left: 40};
+        var width	= optwidth - margin.left - margin.right;
+        var height	= optheight - margin.top - margin.bottom;
+        var yMax = d3.max(dataset, function(d) { return d.high; })
+
+        var x = d3.scaleBand()
+            .rangeRound([0, width])
+            .padding(0.5);
+
+        var y = d3.scaleLinear()
+            .rangeRound([height, 0]);
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Scale the range of the data in the domains
+        x.domain(dataset.map(function(d) { return d.date; }));
+        y.domain([0, yMax + yMax/2]);
+
+  // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+            .data(dataset)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.date); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.high); })
+            .attr("height", function(d) { return height - y(d.high); });
+
+
+        // add the x Axis
+        svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+        // add the y Axis
+        svg.append("g")
+        .call(d3.axisLeft(y));
     }
 
     render() {
